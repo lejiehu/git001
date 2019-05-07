84b0ba86ba00099ef1c0766b37cb3afba0796ca1
lejiehu <1025247813@qq.com>
2019-05-07 20:28:46 +0800

    git提交

---
+++ gitjava/src/cn/itcast/gittest.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("zhongguo");
+        System.out.println("zhongguo");
+        System.out.println("zhongguo");
+        System.out.println("zhongguo");
+    }
+}
